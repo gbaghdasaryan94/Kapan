5b65e811a3707184105e3e08021a013d23dabee2
HaykAmirjanyan <hayk.amirjanyan@mail.ru>
2019-08-22 20:25:34 +0400

    changes

--- HaykAmirjanyan/marioburg.c
+++ HaykAmirjanyan/marioburg.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 int main(void)
 {
-    int heigth = 5;
+    int heigth = get_int("Write int number  ");
     int i, j;
     for ( i = 0; i < heigth; i++ ) {
         for ( j = 0; j < heigth; j++) {
