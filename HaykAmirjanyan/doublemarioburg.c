442167c9af38e86ba00e884e8f1684e3ed5349f8
HaykAmirjanyan <hayk.amirjanyan@mail.ru>
2019-08-22 19:33:16 +0400

    homework week1 double mario burg

---
+++ HaykAmirjanyan/doublemarioburg.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void) {     
+    int heigth = 5;     
+    int i, j;     
+    for(i = 0; i < heigth; i++) {         
+        for(j = 0; j < heigth; j++) {             
+            if(i + j < heigth - 1) {                 
+                printf(" ");
+                } else {                 
+                    printf("#");            
+                    }
+                    }      printf("\t");  
+                for(j = 0; j < heigth; j++){            
+                    if(i + j > heigth - 2 )             
+                    printf("#");         }        
+                    printf("\n");    
+                    }    
+                    
+                }
