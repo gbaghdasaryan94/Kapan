ad63fe3464e86b39645308c0713cfb5ab769656a
Naysgras <sargsyan.mher9@mail.ru>
2019-08-28 12:36:51 +0400

    New

--- Mher/"CS50"/Week "1 Continue"/Porc.c
+++ Mher/"CS50"/Week "1 Continue"/Porc.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+#include <crypt.h>
+
 
 int main(void)
 {
-    string n = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxy";
-    string acryot = "ABFHGRJYSCMKL";
+    string n = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz";
+    char word[5];
+    string pass = "50cNRQBJJ3FXI";
 
     for(int i=0,a=strlen(n); i<a; i++)
     {
-        string key = crypt(n[i],"50");
-        if(key == acryot)
+        word[0]=n[i];
+        string key = crypt(word,"50");
+       
+        if(strcmp(key,pass)==0)
         {
-            printf("%s",n[i]);
+            printf("%c\n",word[0]);
+        }
+        
+        for(int j=0; j<a; j++)
+        {
+            word[1]=n[j];
+            string key = crypt(word,"50");   
+
+            if(strcmp(key,pass)==0)
+            {
+                printf("%c\n",word[0]);
+            }
+
+            for(int r=0; r<a; r++)
+            {
+                word[2]=n[r];
+                string key = crypt(word,"50");
+
+                if(strcmp(key,pass)==0)
+                {
+                    printf("%c\n",word[0]);
+                }
+            }
         }
     }
 }
