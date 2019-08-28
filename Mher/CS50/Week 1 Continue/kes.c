c1503a8c24c84a3ab7e2afac2dabb0a470bf366e
MherSarsyan <sargsyan.mher9@mail.ru>
2019-08-28 20:58:01 +0400

    NEW

--- Mher/CS50/Week 1 Continue/kes.c
+++ Mher/CS50/Week 1 Continue/kes.c
@@ -6,95 +6,28 @@
 
 int main(void)
 {
-    string n = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char word[5];
-    string pass = "50QHu2HtmlTQY";
-    int a=strlen(n);
+    string n = "\0ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    string pass = "50VxqXoSfyiuc";
+    int a=53;
 
     for(int i=0; i<a; i++)
     {
-        word[0]=n[i];
-        string key = crypt(word,"50");
-       
-        if(strcmp(key,pass)==0)
-        {
-            printf("%s\n",word);
-        }      
-    }
-    for(int i=0; i<a; i++)
-    {
-        word[0]=n[i];
         for(int j =0; j<a; j++)
         {
-            word[1]=n[j];
-            string key = crypt(word,"50");
-        
-            if(strcmp(key,pass)==0)
-            {
-                printf("%s\n",word);
-            } 
-        }     
-    }
-    for(int i=0; i<a; i++)
-    {
-        word[0]=n[i];
-        for(int j =0; j<a; j++)
-        {
-            word[1]=n[j];
             for(int t=0; t<a; t++)
             {
-                word[2]=n[t];
-                string key = crypt(word,"50");
-            
-                if(strcmp(key,pass)==0)
-                {
-                    printf("%s\n",word);
-                } 
-            }
-        }     
-    }
-    for(int i=0; i<a; i++)
-    {
-        word[0]=n[i];
-        for(int j =0; j<a; j++)
-        {
-            word[1]=n[j];
-            for(int t=0; t<a; t++)
-            {
-                word[2]=n[t];
                 for(int q=0; q<a; q++)
                 {
-                    word[3]=n[q];
-                    string key = crypt(word,"50");
-                
-                    if(strcmp(key,pass)==0)
-                    {
-                        printf("%s\n",word);
-                    }
-                } 
-            }
-        }     
-    }
-    for(int i=0; i<a; i++)
-    {
-        word[0]=n[i];
-        for(int j =0; j<a; j++)
-        {
-            word[1]=n[j];
-            for(int t=0; t<a; t++)
-            {
-                word[2]=n[t];
-                for(int q=0; q<a; q++)
-                {
-                    word[3]=n[q];
                     for(int x=0; x<a; x++)
                     {
-                        word[4]=n[x];
+                        char word[5] = {n[x],n[q],n[t],n[j],n[i]};
                         string key = crypt(word,"50");
+                        printf("%s\n",word);
                     
                         if(strcmp(key,pass)==0)
                         {
                             printf("%s\n",word);
+                            return 0;
                         }
                     }
                 } 
