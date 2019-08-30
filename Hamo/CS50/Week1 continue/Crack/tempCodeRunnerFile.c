9492f59dacfe5c253bed1439683ca658b2077adc
Hayk Hamo <mkrtchyanh@bk.ru>
2019-08-30 20:04:29 +0400

    change

---
+++ Hamo/CS50/Week1 continue/Crack/tempCodeRunnerFile.c
@@ -0,0 +1,22 @@
+string str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz\n";
+//    int len=strlen(str);
+
+//    int i, j, k, l;
+  
+
+//    for(i=0; i<len; i++){
+//        for(j=0; j<len; j++){
+//            for(k=0; k<len; k++){
+//                for(l=0; l<len;l++){
+                    
+//                     char crack[5]={ str[i],str[j],str[k],str[l] };
+//                 //    if( !strcmp(crypt(crack,"50"),hash) ){             
+//                 //         printf("%s \n",crack);
+//                 //         return 0;
+//                 //    }
+                   
+//                }
+//            }
+//        }
+//    }
+    
