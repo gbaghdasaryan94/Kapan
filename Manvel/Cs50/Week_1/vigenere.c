1b8f8c1c1fe33d4ab1c49b7e6f12dcec049b66d5
man1717 <17.man@mail.ru>
2019-09-02 12:50:58 +0400

    commit4

---
+++ Manvel/Cs50/Week_1/vigenere.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main (void)
+{
+string s = get_string ("input sentance: ");
+string key = get_string ("input key: ");
+int i;
+int j=0;
+int o;
+int p;
+int n = strlen(s);
+int m = strlen(key);
+
+for(i=0;i<n;i++)
+{
+     p = (int)key[j];
+
+    if((int)s[i]>=65 && (int)s[i]<=90)
+    {
+        o=((int)s[i] + p) -97 ;
+        j++; 
+
+        if(o>90)
+        {
+            o-=26;
+        }  
+    }
+    else
+    {
+        if((int)s[i]>=97 && (int)s[i]<=122)
+        {
+            o=(int)s[i] + p -97 ;
+            j++;
+            if(o>122)
+            {
+                o-=26;
+            }
+         }
+        else
+        {
+            o=(int)s[i];
+        }
+    }
+
+    if(j==m-1)
+
+    {
+        j=0;
+    }
+    
+    s[i] = (char)o;
+}
+        printf("%s\n",s);
+
+}
