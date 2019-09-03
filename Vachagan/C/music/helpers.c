1a8de99173861eed4b9f4605ccaa3ff4202879a9
Vachagan <vache.grg40@gmail.com>
2019-09-03 10:48:29 +0400

    New

--- Vachagan/C/music/helpers.c
+++ Vachagan/C/music/helpers.c
@@ -10,8 +10,8 @@
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    int x = fraction[0] - '0';
-    int y = fraction[2] - '0';
+    int x = atoi(&fraction[0]);
+    int y = atoi(&fraction[2]);
 
     return x * (8 / y); 
 }
@@ -19,40 +19,25 @@ int duration(string fraction)
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    char first = note[0];
-    int n = strlen(note);
-    char *notes = "CDEFGB";
-    const int semiton[6] = {9, 7, 5, 4, 2, 2};
-
+    char *notes = "ABCDEFG";
+    int semiton[7] = {0, 2, 9, 7, 5, 4, 2};
     double frequency  = 440.0;
-    int octave = note[n-1] - '0';
-     
-    //  char *a = "Hello World!";
-    // s="bvbrburbhlkvp";
-    int i=strstr(&notes,first)-&notes;
-    // char *b = strstr(notes, note[0]);
+    int octave = atoi(&note[strlen(note)-1]);
 
-    // int i = b - a;
-
-    // for(int i=0; i<6; i++)
-    // {
-    //     if(notes[i] == note[0])
-    //     {
-            if(note[0] == 'B')
-            {
-                frequency *= pow(2.0,semiton[i]/12.0);
-                // continue;
-            }else
+    char *idx = strchr(notes, note[0]);
+    if (idx != NULL) {
+        int i = (int)(idx - notes);
+        if(note[0] == 'B')
+            frequency *= pow(2.0,semiton[i]/12.0);
+        else
             frequency /= pow(2.0,semiton[i]/12.0);
-    //     }
-    // }
+    }
 
     if (octave > 4)
         frequency *= pow(2.0, octave - 4);
     else if(octave < 4)
         frequency /= pow(2.0, 4 - octave);
     
-
     switch (note[1])
     {
         case 'b':
