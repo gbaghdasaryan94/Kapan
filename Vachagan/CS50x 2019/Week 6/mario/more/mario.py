94aa58225eacbb10affee60f50d2f903db6b0a2c
vachagangrigoryan <vache.grg40@gmail.com>
2019-09-17 15:43:42 +0400

    new file or update

--- Vachagan/CS50x 2019/Week 6/mario/more/mario.py
+++ Vachagan/CS50x 2019/Week 6/mario/more/mario.py
@@ -5,5 +5,6 @@ while n < 1 or n > 8:
         n = input("Height: ")
     n = int(n)
 
+
 for i in range(1, n+1):
-    print(" " * (n - i) + "#" * (i))
+    print(" " * (n - i) + "#" * (i) + "  " + "#" * (i))
