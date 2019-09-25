73e07ecac50c2647371505be49067bfdc4d4d09f
vachagangrigoryan <vache.grg40@gmail.com>
2019-09-25 16:22:35 +0400

    new file or update

--- Vachagan/CS50x 2019/Week 6/bleep/bleep.py
+++ Vachagan/CS50x 2019/Week 6/bleep/bleep.py
@@ -4,6 +4,18 @@ from sys import argv
 
 def main():
 
+    if len(argv) != 2:
+        print("Usage: python bleep.py dictionary")
+        exit(1)
+    
+    text = input("What message would you like to censor?")
+
+    file = open(argv[1], "r")
+    
+    dictionary = file.readlines()
+
+    print(text, dictionary)
+
     # TODO
 
 
