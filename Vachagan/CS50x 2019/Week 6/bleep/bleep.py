a806ad4371c577b3cb7ce366b0cc5abf2dd265fa
Vachagan Grigoryan <vachagangrigoryan>
2019-09-26 17:45:14 +0400

    new file or update

--- Vachagan/CS50x 2019/Week 6/bleep/bleep.py
+++ Vachagan/CS50x 2019/Week 6/bleep/bleep.py
@@ -8,15 +8,18 @@ def main():
         print("Usage: python bleep.py dictionary")
         exit(1)
     
-    text = input("What message would you like to censor?")
+    text = input("What message would you like to censor?\n").split()
 
     file = open(argv[1], "r")
     
-    dictionary = file.readlines()
+    dictionary = file.read().split()
 
-    print(text, dictionary)
-
-    # TODO
+    # for i in range(len(text)):
+    #     if text[i].lower() in dictionary:
+    #         text[i] = '*' * len(text[i])
+    
+    text = ['*' * len(word) if word.lower() in dictionary else word for word in text]
+    print(' '.join(text))
 
 
 if __name__ == "__main__":
--- Vachagan/Python/Recursion/generatorArray.py
+++ Vachagan/Python/Recursion/generatorArray.py
@@ -1,6 +1,9 @@
+c = 0
 def generateArray(abc, m, word):
+    global c
     if m==0:
         print(word)
+        c += 1
         return
     
     for leter in abc:
@@ -9,11 +12,15 @@ def generateArray(abc, m, word):
 
 def main():
 
-    abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    # abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    abc = "abcdefg"
     lenA = len(abc)
 
     for i in range(1, lenA + 1):
         generateArray(abc, i, [])
+
+    SUM = [lenA**i for i in range(1,lenA+1)]
+    print(c, sum(SUM), SUM)
 
 if __name__ == "__main__":
     main()
