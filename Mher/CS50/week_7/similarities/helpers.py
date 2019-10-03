644219204fdcfcca4655d2865a63c88841d02e00
MherSarsyan <sargsyan.mher9@mail.ru>
2019-10-03 21:39:49 +0400

    new

--- Mher/CS50/week_7/similarities/helpers.py
+++ Mher/CS50/week_7/similarities/helpers.py
@@ -1,3 +1,7 @@
+from nltk import sent_tokenize
+import nltk
+nltk.download('punkt')
+
 def lines(a, b):
     """Return lines in both a and b"""
 
@@ -8,19 +12,27 @@ def lines(a, b):
     for i in a:
         for j in b:
             if i == j:
+                c = set(j)
 
-    return []
+    return c
 
 
 def sentences(a, b):
     """Return sentences in both a and b"""
 
-    # TODO
-    return []
+    a = sent_tokenize(a)
+    b = sent_tokenize(b)
+    c = []
+    for i in a:
+        for j in b:
+            if i == j:
+                c = set(j)
+
+    return c
 
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
 
-    # TODO
+    
     return []
