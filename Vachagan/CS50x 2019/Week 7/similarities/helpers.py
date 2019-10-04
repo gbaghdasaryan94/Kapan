30f0368dcd14c4047fddbadd2178d4fbf78a71a0
Vachagan Grigoryan <vachagangrigoryan>
2019-10-04 18:59:59 +0400

    new file or update

--- Vachagan/CS50x 2019/Week 7/similarities/helpers.py
+++ Vachagan/CS50x 2019/Week 7/similarities/helpers.py
@@ -1,23 +1,14 @@
 from nltk.tokenize import sent_tokenize
-# import nltk
-# nltk.download('punkt')
+
 
 def lines(a, b):
     """Return lines in both a and b"""
-
     a = set(a.split('\n'))
     b = set(b.split('\n'))
-    
     return a & b
 
-    # a = a.split('\n')
-    # b = b.split('\n')
-    # m = min(len(a), len(b))
-    # print([a[i] for i in range(m) if a[i] == b[i]])
-    # return [a[i] for i in range(m) if a[i] == b[i]]
 
-
-def sentences(a, b): 
+def sentences(a, b):
     """Return sentences in both a and b"""
     a = set(sent_tokenize(a))
     b = set(sent_tokenize(b))
@@ -26,23 +17,6 @@ def sentences(a, b):
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
-
-    # TODO
-    return []
-
-# def substring_tokenize(str, n):
-#     substrings = []
-
-#     for i in range(len(str) - n + 1):
-#         substrings.append(str[i:i + n])
-
-#     return substrings
-
-
-# def substrings(a, b, n):
-#     """Return substrings of length n in both a and b"""
-
-#     a_substrings = set(substring_tokenize(a, n))
-#     b_substrings = set(substring_tokenize(b, n))
-
-#     return a_substrings & b_substrings
+    a = set([a[i:i+n] for i in range(len(a) - n + 1)])
+    b = set([b[i:i+n] for i in range(len(b) - n + 1)])
+    return a & b
