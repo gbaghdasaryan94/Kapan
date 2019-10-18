3e456ad20dcd9f06ad7785424ff05d00479d8ebe
razmikarm <razmikarm@gmail.com>
2019-10-18 16:21:17 +0000

    Some job

--- Final/IMSurvey/static/js/regcheck.js
+++ Final/IMSurvey/static/js/regcheck.js
@@ -1,42 +1,45 @@
 
 $(document).ready(function(){
     $("#name input").focus();
-})
 
-function IsEmail(email) {
-    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})/;
-    if(!regex.test(email)) {
-      return false;
-    }else{
-      return true;
+function fullcheck() {
+    let bad = false;
+    $("input").each(function(){
+        if (!check($(this).val(), $(this).attr("name"))){
+            bad = true;
+        }
+    })
+    if ($("#pword input").val() != $("#cpword input").val() || bad){
+        $("#submit").prop('disabled', true);
+        return false;
     }
+    $("#submit").prop('disabled', false);
 }
 
-$("#email input").on( "keyup", function(){
-    if (IsEmail($(this).val())) {
-        $(this).parent().children("i").attr("class", "fas fa-check-circle");
+function check(name, value) {
+    let regexes = {
+        "email": /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})$/,
+        "name": /^([a-zA-Z\- ])+$/,
+        "pword": /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,20}$/,
+        "cpword": /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,20}$/
+    };
+    return regexes[name].test(value);
+}
+
+$("input").on( "keyup", function(){
+    if (check($(this).attr("name"), $(this).val())){
+        $(this).parent().children("i").attr("class", "fas fa-check-circle");  
+        if ($(this).attr("name") == "cpword" || $(this).attr("name") == "pword"){
+            if ($("#cpword input").val() != $("#pword input").val()) {
+                $("#cpword i").attr("class", "fas fa-exclamation-circle");
+            } else {
+                $("#cpword i").attr("class", "fas fa-check-circle");
+            }
+        }
     } else {
         $(this).parent().children("i").attr("class", "fas fa-exclamation-circle");
     }
+    fullcheck();
 })
 
-// $("#name input").change(function(this){
-//     let name = $(this).val();
-//     if (name) {
-//         $("#email .checked > img").attr("src", "https://icon-library.net/images/checkmark-icon-transparent/checkmark-icon-transparent-10.jpg");
-//     } else {
-//         $("#email .checked > img").attr("src", "https://toppng.com/public/uploads/preview/red-x-in-circle-x-ico-11563249170jvl0jhe7df.png");
-//     }
-// })
-
-$(".register-form").submit(function(e){
-    var empty = false;
-    $('input[type="text"]').each(function(){
-    if($(this).val()=""){
-        empty =true;
-        e.preventDefault(); 
-        alert("Please, fill all fields");
-        return false;
-        }
-    });
 })
