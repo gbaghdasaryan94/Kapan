fbf3f96e3111a87cd01394b1d175a3b2fe13127c
razmikarm <razmikarm@gmail.com>
2019-10-18 16:40:20 +0000

    Add

--- Final/IMSurvey/static/js/regcheck.js
+++ Final/IMSurvey/static/js/regcheck.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
 function fullcheck() {
     let bad = false;
     $("input").each(function(){
-        if (!check($(this).val(), $(this).attr("name"))){
+        if (!check($(this).attr("name"), $(this).val())){
             bad = true;
         }
-    })
+    }) 
     if ($("#pword input").val() != $("#cpword input").val() || bad){
         $("#submit").prop('disabled', true);
         return false;
@@ -20,8 +20,8 @@ function check(name, value) {
     let regexes = {
         "email": /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})$/,
         "name": /^([a-zA-Z\- ])+$/,
-        "pword": /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,20}$/,
-        "cpword": /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,20}$/
+        "pword": /^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{6,20}$/,
+        "cpword": /^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{6,20}$/
     };
     return regexes[name].test(value);
 }
@@ -29,7 +29,7 @@ function check(name, value) {
 $("input").on( "keyup", function(){
     if (check($(this).attr("name"), $(this).val())){
         $(this).parent().children("i").attr("class", "fas fa-check-circle");  
-        if ($(this).attr("name") == "cpword" || $(this).attr("name") == "pword"){
+        if ($(this).attr("type") == "password"){
             if ($("#cpword input").val() != $("#pword input").val()) {
                 $("#cpword i").attr("class", "fas fa-exclamation-circle");
             } else {
