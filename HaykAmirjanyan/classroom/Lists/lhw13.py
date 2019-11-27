900bfd2eebc0630aa7070e671d1821915693a8fd
HaykAmirjanyan <hayk.amirjanyan@mail.ru>
2019-11-27 16:33:05 +0400

    update

--- HaykAmirjanyan/classroom/Lists/lhw13.py
+++ HaykAmirjanyan/classroom/Lists/lhw13.py
@@ -4,8 +4,14 @@
 # 3 7 10
 # 4 8 11 13
 # 5 9 12 14 15
-n = 5
-m = 0
-arr = [[str(j) for i in range(j)]  for j in range(1,n + 1)]
-print("\n".join([' '.join(i) for i in arr]))
 
+n = 6
+arr = []
+for i in range(n):
+    arr.append([])
+    for j in range(n):
+        if j == 0:
+            arr[i].append(str(i + 1))
+        elif j <= i:
+            arr[i].append(str(int(arr[i][j - 1]) + n - j))
+print("\n".join([' '.join(i) for i in arr]))
